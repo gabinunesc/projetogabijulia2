3f441b7766fa2137c8ed508073bab6e7c63f4bc1
Gabriela <143152400+gabinunesc@users.noreply.github.com>
2024-10-15 11:30:36 -0300

    kkl

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
     'Matemática', 
-    'Como se calcula a area de um triângulo?',
-    'Multiplica-se o comprimento e a base e divide por dois'
+    'pergunta 1: Como se calcula a area de um triângulo?',
+    'resposta: Multiplica-se o comprimento e a base e divide por dois'
 )
 
   
