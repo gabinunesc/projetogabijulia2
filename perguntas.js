073a3afc0bb17bb8e521415b088dc9f79df33f43
Gabriela <143152400+gabinunesc@users.noreply.github.com>
2024-10-15 11:44:16 -0300

    jjksoa

--- perguntas.js
+++ perguntas.js
@@ -3,5 +3,98 @@ criaCartao(
     'pergunta 1: Como se calcula a area de um triângulo?',
     'resposta: Multiplica-se o comprimento e a base e divide por dois'
 )
-
-  
+criaCartao(
+    'Matemática', 
+    'pergunta 2: Como se calcula a area de um triângulo?',
+    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+)
+criaCartao(
+    'Matemática', 
+    'pergunta 3: Como se calcula a area de um triângulo?',
+    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+)
+criaCartao(
+    'Matemática', 
+    'pergunta 4: Como se calcula a area de um triângulo?',
+    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+)
+criaCartao(
+    'Matemática', 
+    'pergunta 5: Como se calcula a area de um triângulo?',
+    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+)
+criaCartao(
+    'Matemática', 
+    'pergunta 6: Como se calcula a area de um triângulo?',
+    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+)
+criaCartao(
+    'Matemática', 
+    'pergunta 7: Como se calcula a area de um triângulo?',
+    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+)
+criaCartao(
+    'Matemática', 
+    'pergunta 8: Como se calcula a area de um triângulo?',
+    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+)
+criaCartao(
+    'Matemática', 
+    'pergunta 9: Como se calcula a area de um triângulo?',
+    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+)
+criaCartao(
+    'Matemática', 
+    'pergunta 10: Como se calcula a area de um triângulo?',
+    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+)
+criaCartao(
+    'Matemática', 
+    'pergunta 11: Como se calcula a area de um triângulo?',
+    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+)
+criaCartao(
+    'Matemática', 
+    'pergunta 12: Como se calcula a area de um triângulo?',
+    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+)
+criaCartao(
+    'Matemática', 
+    'pergunta 13: Como se calcula a area de um triângulo?',
+    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+)
+criaCartao(
+    'Matemática', 
+    'pergunta 14: Como se calcula a area de um triângulo?',
+    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+)
+criaCartao(
+    'Matemática', 
+    'pergunta 15: Como se calcula a area de um triângulo?',
+    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+)
+criaCartao(
+    'Matemática', 
+    'pergunta 16: Como se calcula a area de um triângulo?',
+    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+)
+criaCartao(
+    'Matemática', 
+    'pergunta 17: Como se calcula a area de um triângulo?',
+    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+)
+criaCartao(
+    'Matemática', 
+    'pergunta 18: Como se calcula a area de um triângulo?',
+    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+)
+criaCartao(
+    'Matemática', 
+    'pergunta 19: Como se calcula a area de um triângulo?',
+    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+)
+criaCartao(
+    'Matemática', 
+    'pergunta 20: Como se calcula a area de um triângulo?',
+    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+)
