2912a832d63e36c2760b6083ffa5f26d7cc3d861
Gabriela <143152400+gabinunesc@users.noreply.github.com>
2024-10-30 15:11:30 -0300

    aaa

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
     'Matemática', 
-    'pergunta 1: Como se calcula a area de um triângulo?',
-    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+    'pergunta 1: Qual é o valor de x na equação 2x+5=15?',
+    'resposta: x=5'
 )
 criaCartao(
     'Matemática', 
@@ -10,91 +10,91 @@ criaCartao(
 )
 criaCartao(
     'Matemática', 
-    'pergunta 3: Como se calcula a area de um triângulo?',
-    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+    'pergunta 3: Qual é a soma dos ângulos internos de um triângulo?',
+    'resposta: 180'
 )
 criaCartao(
     'Matemática', 
-    'pergunta 4: Como se calcula a area de um triângulo?',
-    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+    'pergunta 4: Calcule a média aritmética dos números 8, 10, 12 e 14.',
+    'resposta: 11 (média: 8+10+12+14/4 = 11'
 )
 criaCartao(
-    'Matemática', 
-    'pergunta 5: Como se calcula a area de um triângulo?',
-    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+    'História', 
+    'pergunta 5: Qual foi a primeira causa da Revolução Industrial?',
+    'resposta: O avanço tecnológico'
 )
 criaCartao(
-    'Matemática', 
-    'pergunta 6: Como se calcula a area de um triângulo?',
-    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+    'História', 
+    'pergunta 6: Em que ano ocorreu a Independência do Brasil?',
+    'resposta: 1822'
 )
 criaCartao(
-    'Matemática', 
-    'pergunta 7: Como se calcula a area de um triângulo?',
-    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+    'História', 
+    'pergunta 7: Qual foi o impacto da Guerra Fria na geopolítica mundial?',
+    'resposta: Divisão do mundo em blocos (capitalista e socialista) e conflitos indiretos'
 )
 criaCartao(
-    'Matemática', 
-    'pergunta 8: Como se calcula a area de um triângulo?',
-    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+    'História', 
+    'pergunta 8: Quem foi Getúlio Vargas?',
+    'resposta: Foi presidente do Brasil. '
 )
 criaCartao(
-    'Matemática', 
-    'pergunta 9: Como se calcula a area de um triângulo?',
-    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+    'Geografia', 
+    'pergunta 9: Quais são os principais biomas brasileiros?',
+    'resposta: Amazônia, cerrado, caatinga, mata atlântica, pampa e pantanal'
 )
 criaCartao(
-    'Matemática', 
-    'pergunta 10: Como se calcula a area de um triângulo?',
-    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+    'Geografia', 
+    'pergunta 10: O que é globalização e quais são suas características?',
+    'resposta: É o processo de interconexão econômica, social e cultural, caracterizado por aumento do comércio e tecnologia.'
 )
 criaCartao(
-    'Matemática', 
-    'pergunta 11: Como se calcula a area de um triângulo?',
-    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+    'Geografia', 
+    'pergunta 11: Qual a principal função do relevo na formação dos climas?',
+    'resposta: Influencia a distribuição de umidade e temperatura.'
 )
 criaCartao(
-    'Matemática', 
-    'pergunta 12: Como se calcula a area de um triângulo?',
-    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+    'Geografia', 
+    'pergunta 12: Quais são os tipos de fronteiras existentes no mundo?',
+    'resposta: Fronteiras naturais, políticas e culturais.'
 )
 criaCartao(
-    'Matemática', 
-    'pergunta 13: Como se calcula a area de um triângulo?',
-    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+    'Português', 
+    'pergunta 13: O que é um texto dissertativo?',
+    'resposta: É um texto que apresenta uma argumentação sobre um tema, com introdução, desenvolvimento e conclusão.'
 )
 criaCartao(
-    'Matemática', 
-    'pergunta 14: Como se calcula a area de um triângulo?',
-    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+    'Português', 
+    'pergunta 14: Qual a função de um advérbio em uma frase?',
+    'resposta: Modificar o verbo, o adjetivo ou outro advérbio, indicando condição como tempo, modo e lugar.'
 )
 criaCartao(
-    'Matemática', 
-    'pergunta 15: Como se calcula a area de um triângulo?',
-    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+    'Português', 
+    'pergunta 15: O que são figuras de linguagem?',
+    'resposta: Recursos expressivos que enriquecem o texto.'
 )
 criaCartao(
-    'Matemática', 
-    'pergunta 16: Como se calcula a area de um triângulo?',
-    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+    'Português', 
+    'pergunta 16: Qual a diferença entre discurso direto e indireto?',
+    'resposta: Discurso direto reproduz as falas exatas, enquanto o indireto narra o conteúdo das falas.'
 )
 criaCartao(
-    'Matemática', 
-    'pergunta 17: Como se calcula a area de um triângulo?',
-    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+    'Biologia', 
+    'pergunta 17: Qual a função da mitocôndria na célula?',
+    'resposta: Produção de energia.'
 )
 criaCartao(
-    'Matemática', 
-    'pergunta 18: Como se calcula a area de um triângulo?',
-    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+    'Biologia', 
+    'pergunta 18: O que é fotossíntese e qual é a sua importância?',
+    'resposta: Processo em que as plantas convertem luz solar em energia química, produzindo oxigênio e glicose.'
 )
 criaCartao(
-    'Matemática', 
-    'pergunta 19: Como se calcula a area de um triângulo?',
-    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+    'BIologia', 
+    'pergunta 19: Qual é a função do sistema imunológico?',
+    'resposta: Defender o organismo contra patógenos e doenças.'
 )
 criaCartao(
-    'Matemática', 
-    'pergunta 20: Como se calcula a area de um triângulo?',
-    'resposta: Multiplica-se o comprimento e a base e divide por dois'
+    'Biologia', 
+    'pergunta 20: O que caracteriza um organismo multicelular?',
+    'resposta: A presença de múltiplas células que se especializam em funções diferentes, formando tecidos e órgãos.'
 )
