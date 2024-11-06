72b08d9e9e5317cdddf029fcbf25d7564e81213e
Gabriela <143152400+gabinunesc@users.noreply.github.com>
2024-11-06 12:12:28 -0300

    jaj

--- pagina2.js
+++ pagina2.js
@@ -1,42 +1,30 @@
-function criaCartao ( categoria, pergunta, resposta){
+function criacartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
-    cartao.innerHTML = `
-
-    <div class="conteudo-cartao">
-    <h3>${categoria}</h3>
-    <div class="pergunta-cartao">
-    <p>${pergunta}</p>
-    </div>
-    <div class="resposta-cartao">
-    <p>${resposta}</p>
-    </div>
-    </div>
-    -
-
-    let respostaEstaVisivel = false;
-
-
-    function viraCartao(){
-     respostaEstaVisivel = !respostaEstaVisivel;
-     cartao.classList.toggle('active', respostaEstaVisivel);
-    }
-
-     cartao.addEventListener('click', viraCartao);
-
-    container.appendChild(cartao)
+    cartao.innerHTML = ` 
+<div class = "conteudo-cartao" onclick="mostraresposta(this) ">
+<h3> ${categoria} </h3>
+<div class="pergunta-cartao">
+<p>${pergunta}</p>   
+</div>
+<div class= "resposta-cartao">
+<p> ${resposta} <p/>   
+</div>
+</div>
+`
+container.appendChild(cartao)
 }
-    function mostraresposta(card){
-    const resposta =card.querySelector('.resposta-cartao);
-    if(resposta.style.display === 'nome'){
-    resposta.style.display = 'block';
+function mostraresposta(card){
+    const resposta =card.querySelector('.resposta-cartao');
+    if(resposta.style.display === 'none'){
+resposta.style.display = 'block';
 
     }
-    else{
-    resposta.style.display= 'nome';
+ else{
+resposta.style.display= 'none';
 
-    }
+ }
 
-    
-    }
+
+}
